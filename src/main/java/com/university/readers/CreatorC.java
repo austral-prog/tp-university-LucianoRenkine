514ec4b4fd472366dcacf64325a291407ebbc6e5
renki <renkineluciano1@gmail.com>
2024-11-21 18:08:53 -0300

    Entrega 3

--- src/main/java/com/university/readers/CreatorC.java
+++ src/main/java/com/university/readers/CreatorC.java
@@ -11,11 +11,14 @@ public class CreatorC implements Creators {
     @Override
     public void create(String parts, University university, Criteria criteriaProcessor) {
         String[] params = parts.split(",");
-        String subjectName = params[0];
-        String criteriaType = params[1];
-        double criteriaValue = Double.parseDouble(params[2]);
-        String[] evaluationNames = Arrays.copyOfRange(params, 3, params.length);
-        Criterion criterion = new Criterion(subjectName, criteriaType, criteriaValue, List.of(evaluationNames));
-        criteriaProcessor.addCriteria(criterion);
+        if (params.length >= 3) {
+            String subjectName = params[0];
+            String criteriaType = params[1];
+            double criteriaValue = Double.parseDouble(params[2]);
+
+            String[] evaluationNames = Arrays.copyOfRange(params, 3, params.length);
+            Criterion criterion = new Criterion(subjectName, criteriaType, criteriaValue, List.of(evaluationNames));
+            criteriaProcessor.addCriteria(criterion);
+        }
     }
 }
